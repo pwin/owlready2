e05569d368d897e0e625ff0227294ac944142a96
Jean-Baptiste "Jiba" Lamy <Jean-Baptiste "Jiba" Lamy@localhost>
2017-06-13 15:15:37 +0200

    add ontology.destroy method

--- namespace.py
+++ namespace.py
@@ -385,6 +385,10 @@ class Ontology(Namespace, _GraphManager):
       if not self.has_triple(self.storid, rdf_type, owl_ontology):
         self.add_triple(self.storid, rdf_type, owl_ontology)
         
+  def destroy(self):
+    del world.ontologies[self.base_iri]
+    self.graph.destroy()
+    
   def _import_changed(self, old):
     old = set(old)
     new = set(self.imported_ontologies)
